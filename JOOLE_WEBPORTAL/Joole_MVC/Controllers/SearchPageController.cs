aed02a067a76c34c516d4a0151a0143a4b4b561d
Aryapranish <pranishstx@gmail.com>
2021-11-01 01:02:52 -0400

    Arya Search Page

---
+++ JOOLE_WEBPORTAL/Joole_MVC/Controllers/SearchPageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Joole_MVC.Controllers
+{
+    public class SearchPageController : Controller
+    {
+        
+        // GET: SearchPage
+        public ActionResult Index(string searchVal, string categoryName)
+        {
+            if(searchVal == null)
+            {
+                searchVal = "Enter a sub category";
+            }
+            if (searchVal.ToLower() == "fans")
+            {
+                return View("~/ProductSummaryController/Fans");
+            }
+            else if (searchVal.ToLower() == "vacuums")
+            {
+                return View("~/ProductSummaryController/Vacuums");
+            }
+            else if (searchVal.ToLower() == "toasters")
+            {
+                return View("~/ProductSummaryController/Toasters");
+            }
+            else
+            {
+              
+                return View();
+
+            }
+        }
+    }
+}
